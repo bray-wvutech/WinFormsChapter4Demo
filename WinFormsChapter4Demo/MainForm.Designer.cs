aa40fcb0c07418a79e86ac861c6e623cab7e8fdd
Brian Ray <bray_stenovations@yahoo.com>
2024-09-15 09:54:44 -0400

    Add project files.

---
+++ WinFormsChapter4Demo/MainForm.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace WinFormsChapter4Demo;
+
+partial class MainForm
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        crustGroupBox = new GroupBox();
+        panRadioButton = new RadioButton();
+        handTossedRadioButton = new RadioButton();
+        thinCrustRadioButton = new RadioButton();
+        toppingsGroupBox = new GroupBox();
+        checkBox4 = new CheckBox();
+        checkBox3 = new CheckBox();
+        checkBox2 = new CheckBox();
+        checkBox1 = new CheckBox();
+        demoListBox = new ListBox();
+        runButton = new Button();
+        label1 = new Label();
+        xTextBox = new TextBox();
+        label2 = new Label();
+        yTextBox = new TextBox();
+        crustButton = new Button();
+        crustLoopButton = new Button();
+        crustGroupBox.SuspendLayout();
+        toppingsGroupBox.SuspendLayout();
+        SuspendLayout();
+        // 
+        // crustGroupBox
+        // 
+        crustGroupBox.Controls.Add(panRadioButton);
+        crustGroupBox.Controls.Add(handTossedRadioButton);
+        crustGroupBox.Controls.Add(thinCrustRadioButton);
+        crustGroupBox.Location = new Point(371, 12);
+        crustGroupBox.Name = "crustGroupBox";
+        crustGroupBox.Size = new Size(175, 148);
+        crustGroupBox.TabIndex = 0;
+        crustGroupBox.TabStop = false;
+        crustGroupBox.Text = "Pizza &Crust";
+        // 
+        // panRadioButton
+        // 
+        panRadioButton.AutoSize = true;
+        panRadioButton.Location = new Point(18, 96);
+        panRadioButton.Name = "panRadioButton";
+        panRadioButton.Size = new Size(53, 24);
+        panRadioButton.TabIndex = 2;
+        panRadioButton.TabStop = true;
+        panRadioButton.Text = "Pan";
+        panRadioButton.UseVisualStyleBackColor = true;
+        // 
+        // handTossedRadioButton
+        // 
+        handTossedRadioButton.AutoSize = true;
+        handTossedRadioButton.Location = new Point(18, 66);
+        handTossedRadioButton.Name = "handTossedRadioButton";
+        handTossedRadioButton.Size = new Size(115, 24);
+        handTossedRadioButton.TabIndex = 1;
+        handTossedRadioButton.TabStop = true;
+        handTossedRadioButton.Text = "Hand Tossed";
+        handTossedRadioButton.UseVisualStyleBackColor = true;
+        // 
+        // thinCrustRadioButton
+        // 
+        thinCrustRadioButton.AutoSize = true;
+        thinCrustRadioButton.Location = new Point(18, 36);
+        thinCrustRadioButton.Name = "thinCrustRadioButton";
+        thinCrustRadioButton.Size = new Size(95, 24);
+        thinCrustRadioButton.TabIndex = 0;
+        thinCrustRadioButton.TabStop = true;
+        thinCrustRadioButton.Text = "Thin Crust";
+        thinCrustRadioButton.UseVisualStyleBackColor = true;
+        // 
+        // toppingsGroupBox
+        // 
+        toppingsGroupBox.Controls.Add(checkBox4);
+        toppingsGroupBox.Controls.Add(checkBox3);
+        toppingsGroupBox.Controls.Add(checkBox2);
+        toppingsGroupBox.Controls.Add(checkBox1);
+        toppingsGroupBox.Location = new Point(642, 12);
+        toppingsGroupBox.Name = "toppingsGroupBox";
+        toppingsGroupBox.Size = new Size(175, 234);
+        toppingsGroupBox.TabIndex = 1;
+        toppingsGroupBox.TabStop = false;
+        toppingsGroupBox.Text = "Pizza &Toppings";
+        // 
+        // checkBox4
+        // 
+        checkBox4.AutoSize = true;
+        checkBox4.Location = new Point(50, 196);
+        checkBox4.Name = "checkBox4";
+        checkBox4.Size = new Size(101, 24);
+        checkBox4.TabIndex = 3;
+        checkBox4.Text = "checkBox4";
+        checkBox4.UseVisualStyleBackColor = true;
+        // 
+        // checkBox3
+        // 
+        checkBox3.AutoSize = true;
+        checkBox3.Location = new Point(47, 151);
+        checkBox3.Name = "checkBox3";
+        checkBox3.Size = new Size(101, 24);
+        checkBox3.TabIndex = 2;
+        checkBox3.Text = "checkBox3";
+        checkBox3.UseVisualStyleBackColor = true;
+        // 
+        // checkBox2
+        // 
+        checkBox2.AutoSize = true;
+        checkBox2.Location = new Point(45, 98);
+        checkBox2.Name = "checkBox2";
+        checkBox2.Size = new Size(101, 24);
+        checkBox2.TabIndex = 1;
+        checkBox2.Text = "checkBox2";
+        checkBox2.UseVisualStyleBackColor = true;
+        // 
+        // checkBox1
+        // 
+        checkBox1.AutoSize = true;
+        checkBox1.Location = new Point(49, 54);
+        checkBox1.Name = "checkBox1";
+        checkBox1.Size = new Size(101, 24);
+        checkBox1.TabIndex = 0;
+        checkBox1.Text = "checkBox1";
+        checkBox1.UseVisualStyleBackColor = true;
+        // 
+        // demoListBox
+        // 
+        demoListBox.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        demoListBox.FormattingEnabled = true;
+        demoListBox.ItemHeight = 31;
+        demoListBox.Items.AddRange(new object[] { "BasicIfSyntaxDemo1", "BasicIfSyntaxDemo2", "BasicIfSyntaxDemo3", "BasicIfSyntaxDemo4", "BasicIfSyntaxDemo5", "ShortCircuitDemo", "StringCompareDemo1", "StringCompareDemo2", "StringCompareDemo3", "DefaultParamValueDemo", "RefParamDemo", "OutParamDemo", "ReturnDemo" });
+        demoListBox.Location = new Point(12, 12);
+        demoListBox.Name = "demoListBox";
+        demoListBox.Size = new Size(250, 314);
+        demoListBox.TabIndex = 2;
+        // 
+        // runButton
+        // 
+        runButton.Location = new Point(12, 440);
+        runButton.Name = "runButton";
+        runButton.Size = new Size(250, 29);
+        runButton.TabIndex = 3;
+        runButton.Text = "&Run Demo";
+        runButton.UseVisualStyleBackColor = true;
+        runButton.Click += runButton_Click;
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Location = new Point(12, 345);
+        label1.Name = "label1";
+        label1.Size = new Size(21, 20);
+        label1.TabIndex = 4;
+        label1.Text = "X:";
+        // 
+        // xTextBox
+        // 
+        xTextBox.Location = new Point(39, 345);
+        xTextBox.Name = "xTextBox";
+        xTextBox.Size = new Size(223, 27);
+        xTextBox.TabIndex = 5;
+        // 
+        // label2
+        // 
+        label2.AutoSize = true;
+        label2.Location = new Point(12, 390);
+        label2.Name = "label2";
+        label2.Size = new Size(20, 20);
+        label2.TabIndex = 6;
+        label2.Text = "Y:";
+        // 
+        // yTextBox
+        // 
+        yTextBox.Location = new Point(42, 392);
+        yTextBox.Name = "yTextBox";
+        yTextBox.Size = new Size(220, 27);
+        yTextBox.TabIndex = 7;
+        // 
+        // crustButton
+        // 
+        crustButton.Location = new Point(373, 171);
+        crustButton.Name = "crustButton";
+        crustButton.Size = new Size(173, 29);
+        crustButton.TabIndex = 8;
+        crustButton.Text = "Crust Demo";
+        crustButton.UseVisualStyleBackColor = true;
+        crustButton.Click += crustButton_Click;
+        // 
+        // crustLoopButton
+        // 
+        crustLoopButton.Location = new Point(375, 217);
+        crustLoopButton.Name = "crustLoopButton";
+        crustLoopButton.Size = new Size(171, 29);
+        crustLoopButton.TabIndex = 9;
+        crustLoopButton.Text = "Crust Loop Demo";
+        crustLoopButton.UseVisualStyleBackColor = true;
+        crustLoopButton.Click += crustLoopButton_Click;
+        // 
+        // MainForm
+        // 
+        AutoScaleDimensions = new SizeF(8F, 20F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(888, 559);
+        Controls.Add(crustLoopButton);
+        Controls.Add(crustButton);
+        Controls.Add(yTextBox);
+        Controls.Add(label2);
+        Controls.Add(xTextBox);
+        Controls.Add(label1);
+        Controls.Add(runButton);
+        Controls.Add(demoListBox);
+        Controls.Add(toppingsGroupBox);
+        Controls.Add(crustGroupBox);
+        Name = "MainForm";
+        Text = "Chapter 4 Demo";
+        Load += MainForm_Load;
+        crustGroupBox.ResumeLayout(false);
+        crustGroupBox.PerformLayout();
+        toppingsGroupBox.ResumeLayout(false);
+        toppingsGroupBox.PerformLayout();
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+
+    private GroupBox crustGroupBox;
+    private RadioButton panRadioButton;
+    private RadioButton handTossedRadioButton;
+    private RadioButton thinCrustRadioButton;
+    private GroupBox toppingsGroupBox;
+    private CheckBox checkBox4;
+    private CheckBox checkBox3;
+    private CheckBox checkBox2;
+    private CheckBox checkBox1;
+    private ListBox demoListBox;
+    private Button runButton;
+    private Label label1;
+    private TextBox xTextBox;
+    private Label label2;
+    private TextBox yTextBox;
+    private Button crustButton;
+    private Button crustLoopButton;
+}
---
+++ WinFormsChapter4Demo/MainForm.cs
@@ -0,0 +1,374 @@
+namespace WinFormsChapter4Demo;
+
+public partial class MainForm : Form
+{
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    private void MainForm_Load(object sender, EventArgs e)
+    {
+    }
+
+    private void DefaultParamValue(int x = 0)
+    {
+        MessageBox.Show(x.ToString());
+    }
+
+    private void DefaultParamValueDemo()
+    {
+        DefaultParamValue(3);
+        DefaultParamValue();
+    }
+
+    private void RefParamFunc(ref int rp)
+    {
+        // doesn't have to be set
+        // if we comment this out everything still compiles
+        rp = 3;
+    }
+
+    private void RefParamDemo()
+    {
+        int x = 4;
+        // does have to be initialized
+        //int x;
+        MessageBox.Show(x.ToString());
+        RefParamFunc(ref x);
+        MessageBox.Show(x.ToString());
+    }
+
+    private void OutParamFunc(out int op)
+    {
+        // does have to be set before returning
+        // if we comment this out we will get an error
+        op = 30;
+    }
+
+    private void OutParamDemo()
+    {
+        int x = 40;
+        // doesn't have to be initialized
+        //int x;
+        MessageBox.Show(x.ToString());
+        OutParamFunc(out x);
+        MessageBox.Show(x.ToString());
+    }
+
+    private bool IsEvenNumber(int x)
+    {
+        return x % 2 == 0;
+    }
+
+    private int GetNumber()
+    {
+        return 1;
+    }
+
+    private void ReturnDemo()
+    {
+        int x = GetNumber();
+
+        if (IsEvenNumber(x))
+        {
+            MessageBox.Show($"{x} is even");
+        }
+        else
+        {
+            MessageBox.Show($"{x} is odd");
+        }
+    }
+
+
+    private void BasicIfSyntaxDemo1(int x, int y)
+    {
+        // basic syntax
+        if (x > 3)
+        {
+            MessageBox.Show($"{x} > 3");
+        }
+        else
+        {
+            MessageBox.Show($"else {x} <= 3");
+        }
+    }
+
+    private void BasicIfSyntaxDemo2(int x, int y)
+    {
+        // one line if statements = bad
+        if (x > 3)
+            MessageBox.Show($"{x} > 3");
+        else
+            MessageBox.Show($"else {x} <= 3");
+    }
+
+    private void BasicIfSyntaxDemo3(int x, int y)
+    {
+        // else if
+        if (x > 3)
+        {
+            MessageBox.Show($"{x} > 3");
+        }
+        else if (x > 1)
+        {
+            MessageBox.Show($"{x} > 1 but <= 3");
+        }
+        else
+        {
+            MessageBox.Show($"{x} <= 1");
+        }
+    }
+
+    private void BasicIfSyntaxDemo4(int x, int y)
+    {
+        // comparison operators
+        if (x == 1)
+        {
+            MessageBox.Show($"{x} == 1");
+        }
+
+        if (x != 1)
+        {
+            MessageBox.Show($"{x} != 1");
+        }
+    }
+
+    private void BasicIfSyntaxDemo5(int x, int y)
+    {
+        // and
+        if ((x < 2) && (y < 2))
+        {
+            MessageBox.Show($"{x} < 2 AND {y} < 2");
+        }
+
+        // or
+        if ((x < 2) || (y < 2))
+        {
+            MessageBox.Show($"{x} < 2 OR {y} < 2");
+        }
+    }
+
+    private void ShortCircuitDemo()
+    {
+        string? str = null;
+
+        // this crashes
+        if (str.Length > 0)
+        {
+            MessageBox.Show("str not empty");
+        }
+
+        // but this does not
+        if ((str != null) && (str.Length > 0))
+        {
+            MessageBox.Show("str is not null or empty");
+        }
+    }
+
+    private void StringCompareDemo1()
+    {
+        string str1 = "abc";
+        string str2 = "xyz";
+        string str3 = "ABC";
+
+        // String.Compare
+        int retval = String.Compare(str1, str2);
+        MessageBox.Show(retval.ToString());
+
+        // case matters
+        retval = String.Compare(str1, str3);
+        MessageBox.Show(retval.ToString());
+
+        // ignore case
+        retval = String.Compare(str1, str3, true);
+        MessageBox.Show(retval.ToString());
+
+        // call from the string object
+        retval = str1.CompareTo(str2);
+        MessageBox.Show(retval.ToString());
+    }
+
+    private void StringCompareDemo2()
+    {
+        string str1 = "abc";
+        string str2 = "xyz";
+        string str3 = "ABC";
+
+        // String.Equals
+        bool isEqual = String.Equals(str1, str3);
+        MessageBox.Show(isEqual.ToString());
+
+        // ignore case
+        isEqual = String.Equals(str1, str3,
+            StringComparison.OrdinalIgnoreCase);
+        MessageBox.Show(isEqual.ToString());
+    }
+
+    private void StringCompareDemo3()
+    {
+        string str1 = "abc";
+        string str2 = "xyz";
+        string str3 = "ABC";
+
+        // another way to ignore case
+        int retval = str1.ToUpper().CompareTo(str3.ToUpper());
+        MessageBox.Show(retval.ToString());
+
+        if (str1.ToUpper() == str2.ToUpper())
+        {
+            MessageBox.Show("strings not equal");
+        }
+    }
+
+    private void runButton_Click(object sender, EventArgs e)
+    {
+        switch (demoListBox.SelectedIndex)
+        {
+            case -1:
+                MessageBox.Show("select a demo");
+                break;
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                int x, y;
+
+                if (!int.TryParse(xTextBox.Text, out x))
+                {
+                    MessageBox.Show("enter a valid value for x");
+                    return;
+                }
+
+                if (!int.TryParse(yTextBox.Text, out y))
+                {
+                    MessageBox.Show("enter a valid vlaue for y");
+                    return;
+                }
+
+                if (demoListBox.SelectedIndex == 0)
+                    BasicIfSyntaxDemo1(x, y);
+                else if (demoListBox.SelectedIndex == 1)
+                    BasicIfSyntaxDemo2(x, y);
+                else if (demoListBox.SelectedIndex == 2)
+                    BasicIfSyntaxDemo3(x, y);
+                else if (demoListBox.SelectedIndex == 3)
+                    BasicIfSyntaxDemo4(x, y);
+                else if (demoListBox.SelectedIndex == 4)
+                    BasicIfSyntaxDemo5(x, y);
+                break;
+            case 5:
+                ShortCircuitDemo();
+                break;
+            case 6:
+                StringCompareDemo1();
+                break;
+            case 7:
+                StringCompareDemo2();
+                break;
+            case 8:
+                StringCompareDemo3();
+                break;
+            case 9:
+                DefaultParamValueDemo();
+                break;
+            case 10:
+                RefParamDemo();
+                break;
+            case 11:
+                OutParamDemo();
+                break;
+            case 12:
+                ReturnDemo();
+                break;
+            default:
+                MessageBox.Show("select a demo");
+                break;
+        }
+    }
+
+    private enum PizzaCrust { ThinCrust, HandTossed, Pan }
+
+    private void crustButton_Click(object sender, EventArgs e)
+    {
+        // an enum is not nullable by default
+        //PizzaCrust crust = null;
+        //PizzaCrust? crust = null;
+
+        PizzaCrust crust = PizzaCrust.ThinCrust;
+
+        // interact with radio buttons through their Checked property
+        if (thinCrustRadioButton.Checked)
+        {
+            crust = PizzaCrust.ThinCrust;
+        }
+
+        if (panRadioButton.Checked)
+        {
+            crust = PizzaCrust.Pan;
+        }
+
+        if (handTossedRadioButton.Checked)
+        {
+            crust = PizzaCrust.HandTossed;
+        }
+
+        // lets examine how enums are stored
+        int crustNumber = (int)crust;
+        MessageBox.Show(crustNumber.ToString());
+        MessageBox.Show(crust.ToString());
+
+        // we often use a switch statement to process values of an enum
+        switch (crust)
+        {
+            case PizzaCrust.ThinCrust:
+                MessageBox.Show("you selected thin crust");
+                break;
+            case PizzaCrust.HandTossed:
+                MessageBox.Show("you selected hand tossed");
+                break;
+            case PizzaCrust.Pan:
+                MessageBox.Show("you selected pan");
+                break;
+            default:
+                MessageBox.Show("unknown crust type");
+                break;
+        }
+
+        // this is a switch EXPRESSION instead of a switch statement
+        string msg = crust switch
+        {
+            PizzaCrust.ThinCrust => "Thin Crust",
+            PizzaCrust.Pan => "Pan",
+            PizzaCrust.HandTossed => "Hand Tossed",
+            _ => "Unknown Crust"
+        };
+        MessageBox.Show(msg);
+    }
+
+    private void crustLoopButton_Click(object sender, EventArgs e)
+    {
+        // loop through each control in the group box
+        foreach (Control c in crustGroupBox.Controls) 
+        {
+            // make sure its a radio button
+            if (c.GetType() == typeof(RadioButton))
+            {
+                // typecast generic control as radio button
+                // so we can access radio button specific properties
+                RadioButton rb = (RadioButton)c;
+                if (rb.Checked)
+                {
+                    MessageBox.Show(rb.Name);
+                    // there should only be one checked at a time
+                    // so you could return here
+                    // but we aren't going to so we can show it loop through all of them
+                }
+            }
+        }
+    }
+}
+
+
+
